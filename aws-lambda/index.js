8db997cdb1264c92df205a471137c30b96b1fb54
Ahmed <ahmed.akod@outlook.com>
2026-07-13 20:56:57 -0400

    feat: add basic script for aws to call gh workflow on cron

---
+++ aws-lambda/index.js
@@ -0,0 +1,31 @@
+import { loadEnvFile } from "node:process"
+import { Octokit } from "octokit"
+
+export async function handler(event) {
+
+  loadEnvFile()
+
+  // Octokit.js
+  // https://github.com/octokit/core.js#readme
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_PAT
+  })
+
+  await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+    owner: "ridgeway-warrior",
+    repo: 'ridgeway-warrior',
+    workflow_id: '310950249',
+    ref: 'master',
+    headers: {
+      'X-GitHub-Api-Version': '2026-03-10'
+    }
+  })
+
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+
+}
