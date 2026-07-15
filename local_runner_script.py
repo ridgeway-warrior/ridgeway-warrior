fe86bcb5a05d039754fba17e25686dd416f959f5
Ahmed <ahmed.akod@outlook.com>
2026-07-14 21:16:29 -0400

    refactor: renamed some files, deleted local test files

--- local_runner_script.py
+++
@@ -1,8 +0,0 @@
-from subprocess import run
-
-from os import chdir
-
-chdir("./video-maker")
-run(["pnpm", "remotion", "render", "MyComp", "../ig-reel-uploader/video.mp4"])
-chdir("../ig-reel-uploader")
-run(["node", "index.js"])
